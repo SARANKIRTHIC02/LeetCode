6f671eb7fd9a48beeeedae60334145a25bd18f02
SARANKIRTHIC02 <sarankirthic1@gmail.com>
2025-05-28 16:25:46 -0400

    stack problem

--- leet2/src/Main.java
+++ leet2/src/Main.java
@@ -1,15 +1,38 @@
+import java.util.Stack;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String s = "abbbabaaa";
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        stack.push(s.charAt(0));
+        for(int i=1;i<s.length();i++)
+        {
+            if(stack.isEmpty())   stack.push(s.charAt(i));
+            else if(stack.peek() == s.charAt(i))
+            {
+                stack.pop();
+            }
+            else
+            {
+                stack.push(s.charAt(i));
+            }
         }
+        for (int i = 0; i < stack.size(); i++) {
+            sb.append(stack.get(i));
+        }
+        String ans = sb.toString();
+        sb.setLength(0);
+        for (int i=ans.length()-1;i>=0;i--)
+        {
+            sb.append(ans.charAt(i));
+        }
+
+        System.out.println(sb.toString());
+        //return sb.toString();
+
     }
 }
